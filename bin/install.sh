885fe566bce88a77714c3071cbcf62e28e876370
default-writer <funcelot@outlook.com>
2025-09-21 00:00:48 +0300

    updates

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -e
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y lsb-release wget gpg gcc make 
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 sudo ./llvm.sh 21 all
-sudo ln -s /usr/bin/lldb-dap-21 /usr/bin/lldb-dap 
+if [[ ! -f "/usr/bin/lldb-dap" ]]; then
+   sudo ln -s /usr/bin/lldb-dap-21 /usr/bin/lldb-dap
+fi
 rm ./llvm.sh
